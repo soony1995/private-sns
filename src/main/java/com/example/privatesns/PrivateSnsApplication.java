d463684606fc318cdca92b85f7b03da66366e372
soony1995 <sanbonpeople@daum.net>
2024-03-11 17:24:24 +0900

    feat: spring 3.2.3 java 17 프로젝트 초기화 프로젝트 초기화

---
+++ src/main/java/com/example/privatesns/PrivateSnsApplication.java
@@ -0,0 +1,13 @@
+package com.example.privatesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateSnsApplication.class, args);
+    }
+
+}
