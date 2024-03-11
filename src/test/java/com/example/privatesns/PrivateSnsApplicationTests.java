c1ff0bb986c133a5dc0cf4447e3d18e6bc0d3b98
soony1995 <sanbonpeople@daum.net>
2024-03-11 17:24:24 +0900

    feat: commit_template

---
+++ src/test/java/com/example/privatesns/PrivateSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.privatesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
